32bb88436ff923dbb084b280c6f198f638955a71
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-02-09 19:50:25 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function updateTime() {
   if (losAngelesElement) {
     let losAngelesDateElement = losAngelesElement.querySelector(".date");
     let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("Europe/London");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
     losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM DD YYYY");
     losAngelesTimeElement.innerHTML = losAngelesTime.format(
